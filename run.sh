8ff01fe8afc26788738f6f047b3d795c6d4297ec
sbonacho <sbonacho@gmail.com>
2018-06-07 16:36:01 +0200

    feat(): add grafana + prometheus

--- run.sh
+++ run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-BASEDIR=$2
+# Dependencies
+
+KAFKA_DOCKER_GIT=https://github.com/wurstmeister/kafka-docker.git
+JMX_AGENT_URL=https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.9/jmx_prometheus_javaagent-0.9.jar
+KAFKA_DASHBOARD_URL=https://raw.githubusercontent.com/prometheus/jmx_exporter/master/example_configs/kafka-0-8-2.yml
+MAP_PLUGIN_URL=https://grafana.com/api/plugins/grafana-worldmap-panel/versions/0.0.21/download
+
+# Definitions
 
 VIRTUALIP="172.16.123.1"
 ORIG=`pwd`
-DIRS="mosquitto-data mosquitto-logs influxdb-data grafana-data kafka-data"
+DIRS="mosquitto-data mosquitto-logs influxdb-data grafana-data kafka-data prometheus-data"
+PROMETHEUS=prometheus/prometheus.jar
+DASHBOARD=prometheus/kafka.yml
 
 # ---------- Defaults --------
-
-if [ "$BASEDIR" == "" ]; then
-    BASEDIR="../demo"
-fi
 
 # ---------- Functions -------
 
@@ -19,6 +24,20 @@ function network(){
     NET=`ifconfig -a|grep ${VIRTUALIP}`
     if [ "$NET" == "" ]; then
         sudo ifconfig lo0 alias ${VIRTUALIP}
+    fi
+}
+
+function downloadKafka(){
+    if [ ! -d "kafka-docker" ]; then
+        git clone $KAFKA_DOCKER_GIT
+    fi
+}
+
+function prometheus(){
+    if [ ! -f "$PROMETHEUS" ]; then
+        mkdir -p prometheus
+        curl $JMX_AGENT_URL -o $PROMETHEUS
+        curl $KAFKA_DASHBOARD_URL -o $DASHBOARD
     fi
 }
 
@@ -58,14 +77,16 @@ function stop(){
 
 # ---------- Script -------
 
-if [ -d "$BASEDIR" ]; then
-    cd $BASEDIR
-fi
-
 case $1 in
     "install")
+        downloadKafka
+        prometheus
+        network
+        createDirs
         ;;
     "start")
+        downloadKafka
+        prometheus
         network
         createDirs
         docker-compose -f "$ORIG/docker-compose.yml" up -d
